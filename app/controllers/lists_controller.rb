73f998229ffa3a2707c42a9a9433204347cdddec
Hayasaka <146614468+kuromame18@users.noreply.github.com>
2023-10-12 08:10:47 +0000

    [Delete] 機能追加の途中

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -18,6 +18,19 @@ class ListsController < ApplicationController
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
   end
 
   private
